d2f22e4f426d6c5fd2b28a3f6a1be404e6c11b4d
Bodin <t.bodin@MacBook-Air-de-Bodin.local>
2022-01-19 02:53:01 +0100

    debug purchasePlaces places and points updates

--- server.py
+++ server.py
@@ -47,6 +47,71 @@ def purchasePlaces():
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     placesRequired = int(request.form['places'])
     competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
+
+
+    # Club file update part
+    # Update number of places and points values by opening clubs json file
+    clubs_data = None
+    with open("clubs.json", "r") as clubs_file:
+        clubs_data = json.load(clubs_file)
+
+        # Find the index of the club in the json data
+        club_index = 0
+        for i in range(len(clubs_data["clubs"])):
+            if clubs_data["clubs"][i]["name"] == club["name"]:
+                club_index = i
+                break
+
+        clubs_data["clubs"][club_index]["points"] -= placesRequired
+        # if the competition already has data in competition, it updates it
+        if clubs_data["clubs"][club_index]["competitions"][competition["name"]]:
+            places_already_purchased = int(clubs_data["clubs"][club_index]["competitions"][competition["name"]]["places_purchased"])
+            clubs_data["clubs"][club_index]["competitions"][competition["name"]]["places_purchased"] = places_already_purchased + placesRequired
+
+        # if competitions data exist but not for this competition
+        elif clubs_data["clubs"][club_index]["competitions"]:
+            data_competition = {"places_purchased": places_required}
+            clubs_data["clubs"][club_index]["competitions"][competition["name"]] = data_competition
+
+        ## if no data it create it, with a list of dictionaries of commpetitions with name and place purchased
+        else:
+            competitions_dict = {}
+            data_competition = {"places_purchased": placesRequired}
+            competitions_dict[competition["name"]] = data_competition
+            clubs_data["clubs"][club_index]["competitions"] = competitions_dict
+
+        clubs_file.close()
+
+    # Update the values in the clubs json file
+    with open("clubs.json", "w") as clubs_file:
+        clubs_file.write(json.dumps(clubs_data))
+        clubs_file.close()
+
+
+    # Competition file update
+    # Update places available value by opening the json file
+    competitions_data = None
+    with open("competitions.json", "r") as competitions_file:
+
+        competitions_data = json.load(competitions_file)
+
+        # Find the index of the competition in the json data
+        competition_index = 0
+        for i in range(len(competitions_data)):
+            if competition_data["competitions"][i]["name"] == competition["name"]:
+                competition_index = i
+                break
+
+        numberOfPlaces = int(competitions_data["competitions"][competition_index]["numberOfPlaces"])
+        competitions_data["competitions"][competition_index]["numberOfPlaces"] = numberOfPlaces - placesRequired
+
+
+    # Update the values in the competitions json file
+    with open("competitions.json", "w") as competitions_file:
+        competitions_file.write(json.dumps(competitions_data))
+        competitions_file.close()
+
+
     flash('Great-booking complete!')
     return render_template('welcome.html', club=club, competitions=competitions)
 
